48890305069243063d4bf88f6d222a5721cbe907
erkankavas <erkankavas@gmail.com>
2023-10-07 15:13:41 +0300

    Add existing project files to Git

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+const port = 3000;
+dotenv.config();
+
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+
+
+let title = "nWeather";
+let apiKey = process.env.API_KEY;
+
+if (req.query.city == undefined) {
+  var city = "Antalya"
+} else {
+  var city = req.query.city
+}
+
+
+let api = "https://api.openweathermap.org/data/2.5/weather?appid="+apiKey+"&q="+city+"&units=metric";
+
+  fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+  
+     res.render("index.ejs", {
+       title: title,
+    data: data,
+  });
+
+       });
+
+  });
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
